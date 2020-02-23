027b3a1af1e2b298bbf1893f9b434535aee0ce23
akarshit <akarshitsrivastava33@gmail.com>
2020-02-24 00:07:40 +0530

    add method is done

--- src/Defination/linkedlist.java
+++ src/Defination/linkedlist.java
@@ -3,10 +3,42 @@ package Defination;
 import adt.UseAdt;
 
 public class linkedlist<E> implements UseAdt<E> {
+    private Node head = null;
+    private int size = 0;
+
+    public Node getIndex(int index) {
+        Node response = head;
+        for (int i = 0; i < index; i++) {
+            response = response.getNext();
+        }
+        return response;
+    }
+
+    private void addFirst(Person item) {
+        head = new Node(item, head);
+        size = size + 1;
+
+    }
+
+    private void addAfter(Node node, Person item) {
+        node.next = new Node(item, node.next);
+        size = size + 1;
+    }
+
+    private void add(int index, Person item) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException(Integer.toString(index));
+        } else if (index == 0) {
+            addFirst(item);
+        } else {
+            Node temp = getIndex(index - 1);
+            addAfter(temp, item);
+        }
+    }
 
     @Override
     public void add(Person item) {
-
+        add(size, item);
     }
 
     @Override
