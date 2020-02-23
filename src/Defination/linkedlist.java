54dd0f7a7f1e5f9d96d5d0460affe47e1aa4e83f
akarshit <akarshitsrivastava33@gmail.com>
2020-02-24 00:05:12 +0530

    Node class in linklist hasbeen created.

--- src/Defination/linkedlist.java
+++ src/Defination/linkedlist.java
@@ -3,6 +3,7 @@ package Defination;
 import adt.UseAdt;
 
 public class linkedlist<E> implements UseAdt<E> {
+
     @Override
     public void add(Person item) {
 
@@ -26,5 +27,27 @@ public class linkedlist<E> implements UseAdt<E> {
     @Override
     public void show() {
 
+    }
+
+    private static class Node {
+        private Person data;
+        private Node next;
+
+        public Node(Person data) {
+            this.data = data;
+        }
+
+        public Node(Person data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public Person getData() {
+            return data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
     }
 }
